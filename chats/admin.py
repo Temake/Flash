5fa92103b0f40819692de259c59efa1120d9758a
Temake <teminioluwaopemipo@gmail.com>
2025-10-03 00:00:00 +0100

    Fix:Admin

--- chats/admin.py
+++ chats/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Chat)
+admin.site.register(Conversation)
+admin.site.site_header = "Chat Administration"
+admin.site.site_title = "Chat Admin Portal"
